b5b2ed5deaf582a99762d36dac03b2d9d5964d04
PratyayChakraborty <pratyaychakraborty024@gmail.com>
2022-07-20 14:12:04 +0530

    add navbar 1

---
+++ index.js
@@ -0,0 +1,7 @@
+import {navbar,navjs,cross,features}  from "./navbar.js"
+console.log("hello")
+// document.getElementById("navbar1").style.backgroundColor="red"
+document.getElementById("navbar1").innerHTML = navbar()
+navjs()
+cross()
+features()
