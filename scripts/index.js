947fb4bc2482a63ad9edd51ab99aa1993c378a2c
PratyayChakraborty <pratyaychakraborty024@gmail.com>
2022-07-22 21:57:42 +0530

    added 2nd nav & homepage

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,12 @@ import { navbar, navjs, cross, features } from "../components/navbar.js"
 import{footer} from "../components/footer.js"
 console.log("hello")
 // document.getElementById("navbar1").style.backgroundColor="red"
-document.getElementById("navbar1").innerHTML = navbar();
+document.getElementById("navbar").innerHTML = navbar();
 document.getElementById("footer").innerHTML = footer();
 navjs()
 cross()
 features()
+let userdata = JSON.parse(localStorage.getItem('userdata'))||[];
+let name = userdata[0].name || "";
+    
+document.getElementById("usename").innerText = name;
